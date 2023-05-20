11604b8402b0a1f0725c15833b32586a5a6c1789
Andrea Fernández <andreafernez@estudiantec.cr>
2023-05-19 19:35:04 -0600

    add contrAndroid.cpp y contrAndroid.h para el giroscopio

---
+++ PuckMan/contrAndroid.h
@@ -0,0 +1,12 @@
+#ifndef PROYECTO_2_PACMAN_CONTRANDROID_H
+#define PROYECTO_2_PACMAN_CONTRANDROID_H
+
+using namespace std;
+
+class Controller{
+
+public:
+    void SocketServer();
+};
+
+#endif //PROYECTO_2_PACMAN_CONTRANDROID_H
